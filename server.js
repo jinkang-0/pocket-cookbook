68b3b19461d550b367af4f98c3b9f2801cb45b20
jinkang-0 <jinkang.fang@gmail.com>
2022-07-11 23:43:30 -0700

    readjusting proxy

--- server.js
+++ server.js
@@ -10,16 +10,18 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // setup db
 const dbo = require('./connect');
 app.use(require('./routes/recipes'));
 
-// view routes
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-})
+// production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, "./client/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    });
+}
 
 // start server
 app.listen(port, () => {
