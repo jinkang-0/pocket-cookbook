df8d34f90834bf7845e9af12642a134cf685ced2
jinkang-0 <jinkang.fang@gmail.com>
2022-07-12 19:03:49 -0700

    edited routes to use router params

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require('dotenv').config({ path: './config.env' });
 const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const path = require('path');
 const app = express();
@@ -10,53 +9,10 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-// setup db
-const uri = process.env.ATLAS_URI;
-const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1
-});
-const db = client.db("pocket-cookbook");
-
-async function connectDb() {
-    // connect
-    await client.connect();
-
-    // establish and verify connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Successfully connected to MongoDB.");
-}
-
 // db routes
-app.get('/api/recipes', async (req, res) => {
-    const data = await db.collection('recipes').find().sort({ name: 1 }).toArray();
-    res.json(data);
-});
-
-app.get('/api/verification', async (req, res) => {
-    const data = await db.collection('misc').findOne({ data: "verificationCode" });
-    res.json(data);
-});
-
-app.get('/api/recipes/:id', (req, res) => {
-    try {
-        const id = ObjectId(req.param.id);
-        db.collection('recipes').findOne({ _id: id }, (err, data) => {
-            if (err)
-                throw err;
-            res.json(data);
-        });
-    } catch {
-        res.sendStatus(404);
-    }
-});
-
-app.post('/api/recipes/add', async (req, res) => {
-    await db.collection('recipes').insertOne(req.body);
-    res.sendStatus(200);
-});
-
+const connectDb = require('./connect').connect;
+app.use('/api/recipes', require('./api/recipes'));
+app.use('/api/verification', require('./api/verification'));
 
 // production
 if (process.env.NODE_ENV === 'production') {
@@ -68,7 +24,10 @@ if (process.env.NODE_ENV === 'production') {
 
 // start server
 app.listen(port, () => {
-    connectDb();
+    connectDb((err) => {
+        if (err)
+            console.log("Error connecting to MongoDB:", err);
+    });
     console.log("Server is running on port", port);
 });
 
