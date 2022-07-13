cb84e5a6c74a1a424fbfc63519f00456ddbe6fb7
jinkang-0 <jinkang.fang@gmail.com>
2022-07-12 18:45:30 -0700

    changed scripts and structure for deployment

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config({ path: './config.env' });
-
-// setup express
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const path = require('path');
 const app = express();
@@ -12,8 +11,52 @@ app.use(express.json());
 app.use(cors());
 
 // setup db
-const dbo = require('./connect');
-app.use(require('./api/recipes'));
+const uri = process.env.ATLAS_URI;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
+const db = client.db("pocket-cookbook");
+
+async function connectDb() {
+    // connect
+    await client.connect();
+
+    // establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB.");
+}
+
+// db routes
+app.get('/api/recipes', async (req, res) => {
+    const data = await db.collection('recipes').find().sort({ name: 1 }).toArray();
+    res.json(data);
+});
+
+app.get('/api/verification', async (req, res) => {
+    const data = await db.collection('misc').findOne({ data: "verificationCode" });
+    res.json(data);
+});
+
+app.get('/api/recipes/:id', (req, res) => {
+    try {
+        const id = ObjectId(req.param.id);
+        db.collection('recipes').findOne({ _id: id }, (err, data) => {
+            if (err)
+                throw err;
+            res.json(data);
+        });
+    } catch {
+        res.sendStatus(404);
+    }
+});
+
+app.post('/api/recipes/add', async (req, res) => {
+    await db.collection('recipes').insertOne(req.body);
+    res.sendStatus(200);
+});
+
 
 // production
 if (process.env.NODE_ENV === 'production') {
@@ -25,10 +68,7 @@ if (process.env.NODE_ENV === 'production') {
 
 // start server
 app.listen(port, () => {
-    dbo.connect((err) => {
-        if (err)
-            console.error(err);
-    });
+    connectDb();
     console.log("Server is running on port", port);
 });
 
